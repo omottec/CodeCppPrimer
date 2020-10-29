cb35129a47a3a7f2ae6d470ef68dec8003955d2a
qinbingbing <qinbingbing@bytedance.com>
2020-10-29 19:39:22 +0800

    dynamically allocated & smart pointer

---
+++ char12/useBlob.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "StrBlob.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    StrBlob b1;
+    {
+        StrBlob b2 = { "a", "an", "the" };
+        b1 = b2;
+        b2.push_back("about");
+        cout << b2.size() << endl;
+    }
+    cout << b1.size() << endl;
+    for (auto it = b1.begin(); neq(it, b1.end()); it.incr())
+        cout << it.deref() << endl;
+    return 0;
+}
